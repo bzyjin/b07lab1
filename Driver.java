50bf84844a72d07714a58ed5bd4be9eaa63e5a7c
Bill Jin <149437563+bzyjin@users.noreply.github.com>
2024-06-02 17:49:24 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,42 @@
+import java.io.File;
+
 public class Driver {
     public static void main(String [] args) {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+
+        double[] c1 = {6, 5};
+        int[] e1 = {0, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+
+        double[] c2 = {-9, -2};
+        int[] e2 = {4, 1};
+        Polynomial p2 = new Polynomial(c2, e2);
+
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
+
         if(s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+
+        p = p2.multiply(p1);
+
+        try {
+            File poly = new File("poly.txt");
+            Polynomial g = new Polynomial(poly);
+            System.out.println("Loaded poly.txt");
+        } catch (Exception e) {
+            System.out.println("Could not load poly.txt");
+            // e.printStackTrace();
+        }
+
+        try {
+            p.saveToFile("out.txt");
+            System.out.println("Saved p to out.txt");
+        } catch (Exception e) {
+            System.out.println("Could not save p to out.txt");
+        }
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,37 +1,215 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Polynomial {
-    double[] coefficients;
+    double[] c;
+    int[] e;
 
     public Polynomial() {
-        this.coefficients = new double[1];
+        this.c = null;
+        this.e = null;
     }
 
-    public Polynomial(double[] coefficients) {
-        this.coefficients = new double[coefficients.length];
-        for (int i = 0; i < coefficients.length; ++i) {
-            this.coefficients[i] = coefficients[i];
+    public Polynomial(double[] c, int[] e) {
+        this();
+        if (c != null && e != null) {
+            assert c.length == e.length;
+
+            this.c = c.clone();
+            this.e = e.clone();
         }
     }
 
-    public Polynomial add(Polynomial that) {
-        boolean thisLarger = this.coefficients.length > that.coefficients.length;
-        double[] large = thisLarger ? this.coefficients : that.coefficients;
-        double[] small = thisLarger ? that.coefficients : this.coefficients;
+    public Polynomial(String s) {
+        s = s   .replaceAll("-", "+-")
+                .replaceAll("\\+x", "+1x")
+                .replaceAll("-x", "-1x")
+                .replaceAll("x", "x ");
+        if (s.startsWith("x")) s = "1" + s;
+        String[] tokens = s.split("\\+");
 
-        Polynomial res = new Polynomial(large);
-        for (int i = 0; i < small.length; ++i) {
-            res.coefficients[i] += small[i];
+        int len = tokens.length;
+        double[] c = new double[len];
+        int[] e = new int[len];
+
+        for (int i = 0; i < len; ++i) {
+            String[] constants = tokens[i].split("x");
+
+            c[i] = Double.parseDouble(constants[0]);
+            if (constants.length < 2) e[i] = 0;
+            else if (constants[1].equals(" ")) e[i] = 1;
+            else e[i] = Integer.parseInt(constants[1].trim());
+        }
+
+        this.c = c;
+        this.e = e;
+    }
+
+    public Polynomial(File file) throws FileNotFoundException {
+        this((new Scanner(file)).nextLine());
+    }
+
+    public void saveToFile(String path) throws IOException {
+        FileWriter f = new FileWriter(path);
+
+        if (this.len() == 0) {
+            f.write("0");
+        }
+
+        else {
+            StringBuilder fmt = new StringBuilder();
+            for (int i = 0; i < this.len(); ++i) {
+                if (i > 0 && this.c[i] >= 0) fmt.append("+");
+                fmt.append(String.valueOf(this.c[i]));
+
+                if (this.e[i] > 0) {
+                    fmt.append("x");
+                    if (this.e[i] > 1) {
+                        fmt.append(String.valueOf(this.e[i]));
+                    }
+                }
+            }
+
+            f.write(fmt.toString());
+        }
+
+        f.close();
+    }
+
+    private Polynomial addUnsafe(Polynomial that) {
+        if (that.len() == 0) return new Polynomial(this.c, this.e);
+        if (this.len() == 0) return new Polynomial(that.c, that.e);
+
+        int len = this.len() + that.len();
+        int[] e = new int[len];
+        double[] c = new double[len];
+
+        Polynomial  a = this, b = that;
+
+        int i = 0, p2 = 0;
+        for (int p1 = 0; p1 < a.len(); ++p1) {
+            while (p2 < b.len() && b.e[p2] <= a.e[p1]) {
+                c[i] += b.c[p2];
+                e[i++] = b.e[p2++];
+            }
+
+            if (p2 > 0 && b.e[p2 - 1] == a.e[p1]) --i;
+
+            c[i] += a.c[p1];
+            e[i++] = a.e[p1];
+        }
+
+        while (p2 < b.len()) {
+            c[i] += b.c[p2];
+            e[i++] += b.e[p2++];
+        }
+
+        return new Polynomial(c, e)
+                .withFilteredTerms();
+    }
+
+    public Polynomial add(Polynomial that) {
+        return this.ordered().addUnsafe(that.ordered());
+    }
+
+    public Polynomial subtract(Polynomial that) {
+        return this.add(that.negated());
+    }
+
+    public Polynomial withFilteredTerms() {
+        if (this.len() == 0) return new Polynomial();
+
+        int numZeros = 0;
+        for (int i = 0; i < this.c.length; ++i) {
+            if (Math.abs(this.c[i]) <= 1e-9) ++numZeros;
+        }
+
+        double[] c = new double[this.len() - numZeros];
+        int[] e = new int[c.length];
+
+        if (c.length == 0) return new Polynomial();
+
+        int j = 0;
+        for (int i = 0; i < this.c.length; ++i) {
+            if (Math.abs(this.c[i]) <= 1e-9) continue;
+
+            c[j] = this.c[i];
+            e[j] = this.e[i];
+            ++j;
+        }
+
+        return new Polynomial(c, e);
+    }
+
+    public Polynomial negated() {
+        if (this.len() == 0) return new Polynomial();
+
+        double[] c = this.c.clone();
+        for (int i = 0; i < c.length; ++i) {
+            c[i] *= -1;
+        }
+
+        return new Polynomial(c, this.e);
+    }
+
+    public Polynomial multiply(Polynomial that) {
+        if (this.len() == 0 || that.len() == 0) return new Polynomial();
+
+        Polynomial res = new Polynomial();
+        Polynomial  a = this.ordered(),
+                    b = that.ordered();
+
+        for (int i = 0; i < this.e.length; ++i) {
+            int[] e = b.e.clone();
+            double[] c = b.c.clone();
+
+            for (int j = 0; j < e.length; ++j) {
+                e[j] += a.e[i];
+                c[j] *= a.c[i];
+            }
+
+            res = res.addUnsafe(new Polynomial(c, e));
         }
 
         return res;
     }
 
+    public Polynomial ordered() {
+        if (this.len() == 0) return new Polynomial();
+
+        double[] c = new double[this.c.length];
+        int[] e = new int[this.e.length];
+
+        // Insertion sort
+        for (int i = 0; i < this.len(); ++i) {
+            int j = i;
+
+            double c_val = this.c[i];
+            int e_val = this.e[i];
+
+            while (j > 0 && e[j - 1] > e_val) {
+                c[j] = c[j - 1];
+                e[j] = e[j - 1];
+                --j;
+            }
+
+            c[j] = c_val;
+            e[j] = e_val;
+        }
+
+        return new Polynomial(c, e);
+    }
+
     public double evaluate(double x) {
-        double power = 1.;
+        if (this.len() == 0) return 0.;
         double res = 0.;
 
-        for (int i = 0; i < this.coefficients.length; ++i) {
-            res += power * this.coefficients[i];
-            power *= x;
+        for (int i = 0; i < this.len(); ++i) {
+            res += this.c[i] * Math.pow(x, this.e[i]);
         }
 
         return res;
@@ -39,5 +217,10 @@ public class Polynomial {
 
     public boolean hasRoot(double x) {
         return Math.abs(this.evaluate(x)) <= 1e-9; // EPSILON
+    }
+
+    public int len() {
+        if (this.c == null || this.e == null) return 0;
+        return this.c.length;
     }
 }
